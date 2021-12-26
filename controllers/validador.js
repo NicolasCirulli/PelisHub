95dcb6efbb3c50f8864da00d74babd7e037cc1fe
JoaquinNunezCid <nunezcidjoaquin@gmail.com>
2021-12-26 19:19:12 -0300

    validador en español

--- controllers/validador.js
+++ controllers/validador.js
@@ -3,21 +3,21 @@ const joi = require('joi')
 const validador = (req, res, next) => {
     const schema = joi.object({
         nombre:joi.string().trim().min(2).required().messages({
-            'string.empty' : 'You must complete this field',
-            'string.min' : 'Invalid name',
+            'string.empty' : 'Debes completar este campo',
+            'string.min' : 'nombre no permitido',
         }),
         apellido:joi.string().trim().min(2).required().messages({
-            'string.empty' : 'You must complete this field',
-            'string.min' : 'Invalid Last name',
+            'string.empty' : 'Debes completar este campo',
+            'string.min' : 'apellido no permitido',
         }),
         mail:joi.string().trim().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required().messages({
-            'string.email' : 'You must use a valid email account'
+            'string.email' : 'Debes usar una cuenta de mail valida'
         }),
         contrasenia:joi.string().min(5).trim().required().messages({
-            'string.empty' : 'You must complete this field',
+            'string.empty' : 'Debes completar este campo',
         }),
         foto:joi.string().min(5).trim().required().messages({
-            'string.min': 'Please use a valid url'
+            'string.min': 'Introduzca una direccion valida'
         }),
         google:joi.boolean(),
     })
