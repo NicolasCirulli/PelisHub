644bbeb99931ca0d639ca0cd0d48e726d2ce31fb
Nicolas Catriel Cirulli <nicolascirulli2@gmail.com>
2021-12-27 11:10:34 -0300

    paginacion completa

--- frontend/src/pages/Peliculas.js
+++ frontend/src/pages/Peliculas.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import {Container,Pagination} from 'react-bootstrap'
+import { Container, Pagination } from "react-bootstrap";
 import CardPeliculas from "../components/CardPeliculas/CardPeliculas";
-
 
 const Peliculas = () => {
   const [peliculasDefault, setPeliculasDefault] = useState();
@@ -12,46 +11,57 @@ const Peliculas = () => {
   const [generoSeleccionado, setGeneroSeleccionado] = useState(28);
   const [sortBy, setSortBy] = useState("popularity.desc");
   const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1)
+  const [totalPages, setTotalPages] = useState(1);
   // referencia
   const buscadorPorNombre = useRef();
-
-
 
   // paginacion
 
   let items = [];
-  for (let number = 1; number <= 10; number++) {
-    items.push(
-      <Pagination.Item key={number} active={number === page} onClick={()=> setPage(number)}>
-        {number}
-      </Pagination.Item>
-    );
+  for (let number = page; number <= page + 5; number++) {
+    number <= totalPages &&
+      items.push(
+        <Pagination.Item
+          key={number}
+          active={number === page}
+          onClick={() => setPage(number)}
+        >
+          {number}
+        </Pagination.Item>
+      );
   }
 
+  useEffect(() => {
+    fetchear();
+    buscadorPorNombre.current.value = "";
+  }, [page]);
 
-  console.log(totalPages)
+  useEffect(() => {
+    fetchear();
+    setPage(1);
+    buscadorPorNombre.current.value = "";
+  }, [generoSeleccionado]);
 
-    
-
-
-
-    // const api = 'https://api.themoviedb.org/3/discover/movie?api_key=43fd83d3a9756a2f59b0de39480b3bf7&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'
-
-    const urlApi = 'https://api.themoviedb.org/3/'
-    const apiKey = '43fd83d3a9756a2f59b0de39480b3bf7'
+  const urlApi = "https://api.themoviedb.org/3/";
+  const apiKey = "43fd83d3a9756a2f59b0de39480b3bf7";
 
   // component did mount
   useEffect(async () => {
     try {
-      const res = await axios.get(`${urlApi}discover/movie?api_key=${apiKey}&language=es-ES&sort_by=${sortBy}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`);
-        console.log(res.data)
+      const res = await axios.get(
+        `${urlApi}discover/movie?api_key=${apiKey}&language=es-ES&sort_by=${sortBy}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+      );
+      console.log(res.data);
       setPeliculas(res.data.results);
       setPeliculasDefault(res.data.results);
 
-      const generos = await axios.get(`${urlApi}genre/movie/list?api_key=${apiKey}&language=es-MX`);
+      const generos = await axios.get(
+        `${urlApi}genre/movie/list?api_key=${apiKey}&language=es-MX`
+      );
       setGenero(generos.data.genres);
-    } catch (err) {console.log(err);}
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
 
   // FUNCIONES
@@ -62,7 +72,11 @@ const Peliculas = () => {
       const res = await axios.get(
         `${urlApi}discover/movie?api_key=${apiKey}&language=es-ES&sort_by=${sortBy}&include_adult=false&include_video=false&page=${page}&with_genres=${generoSeleccionado}&with_watch_monetization_types=flatrate&language=es-MX`
       );
-      setTotalPages(res.data.total_pages);
+      if (res.data.total_pages < 450) {
+        setTotalPages(res.data.total_pages);
+      } else {
+        setTotalPages(450);
+      }
       setPeliculas(res.data.results);
     } catch (err) {
       console.log(err);
@@ -76,7 +90,6 @@ const Peliculas = () => {
         const res = await axios.get(
           `https://api.themoviedb.org/3/search/multi?api_key=43fd83d3a9756a2f59b0de39480b3bf7&query=${value}&&language=es-MX`
         );
-        console.log(res.data.results)
         setPeliculas(res.data.results);
       } catch (err) {
         console.log(err);
@@ -105,19 +118,27 @@ const Peliculas = () => {
           </select>
         </form>
       </div>
-     
-      <div className="container-fluid d-flex justify-content-center">
-        <Pagination>{items}</Pagination>
-      </div>
 
       <div className="d-flex flex-wrap justify-content-center">
         {peliculas.map((pelicula) => {
-          if (pelicula.overview && pelicula.backdrop_path) {
-            return <CardPeliculas key={pelicula.id} datos={pelicula} />;
-          }
+          return <CardPeliculas key={pelicula.id} datos={pelicula} />;
         })}
       </div>
-
+        {totalPages > 1 && (
+          <div className="container-fluid d-flex justify-content-center">
+            <Pagination>
+              <Pagination.First onClick={() => setPage(1)} />
+              <Pagination.Prev
+                onClick={() => setPage(page < 2 ? page : page - 1)}
+              />
+              {items}
+              <Pagination.Next
+                onClick={() => setPage(page > totalPages ? page : page + 1)}
+              />
+              <Pagination.Last onClick={() => setPage(totalPages - 5)} />
+            </Pagination>
+          </div>
+        )}
     </Container>
   );
 };
