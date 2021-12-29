c43d7528e90f8ea34df59cfdf116b774244054cd
CarlosV319 <carlos.alberto_88@hotmail.com>
2021-12-28 21:17:09 -0300

    carrusel en inicio

---
+++ frontend/src/components/CardPeliculas/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+
+const Card = () => {
+  let imagen;
+  if (datos.backdrop_path) {
+    imagen = `https://image.tmdb.org/t/p/w500/${datos.backdrop_path}`;
+  } else {
+    imagen = "./assets/nodisponible.png";
+  }
+
+  return (
+    <>
+      <div className="wrapper">
+  <div className="app">
+    <div className="card">
+      <img src={imagen} alt="" />
+      <div>
+        <h2>{datos.title || datos.original_name}</h2>
+        <p>kjandwknbakwbndb</p>
+      </div>
+    </div>
+  </div>
+</div>;
+
+    </>
+  );
+};
+
+export default Card;
