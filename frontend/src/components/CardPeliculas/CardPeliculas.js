a2744579fb3a7f8187049c40d2cb425962d63027
Nicolas Catriel Cirulli <nicolascirulli2@gmail.com>
2021-12-30 00:48:19 -0300

    estilos page pelicula

--- frontend/src/components/CardPeliculas/CardPeliculas.js
+++ frontend/src/components/CardPeliculas/CardPeliculas.js
@@ -5,17 +5,31 @@ import {Card, Button} from 'react-bootstrap'
 const CardPeliculas = ({ datos }) => {
 
     let imagen;
+    let estilos;
     if(datos.backdrop_path){
        imagen = `https://image.tmdb.org/t/p/w500/${datos.backdrop_path}`
+      }else if(datos.poster_path){
+      imagen = `https://image.tmdb.org/t/p/w500/${datos.poster_path}`
+      estilos = {
+        overflow: 'hidden',
+        objectFit: 'cover',
+        flex: 6
+      }
     }else{
       imagen = './assets/nodisponible.png' 
+      estilos = {
+        overflow: 'hidden',
+        objectFit: 'cover',
+        flex: 6
+      }
+
     }
 
 
   return (
     <>
       <Card className="card-pelis">
-        <Card.Img variant="top" src={imagen} />
+        <Card.Img variant="top" src={imagen} style={estilos} />
         <Card.Body>
           <Card.Title className="tituloPelicula">{datos.title || datos.original_name }</Card.Title>
         </Card.Body>
